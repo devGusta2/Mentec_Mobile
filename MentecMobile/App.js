799456deb98654bc9cbcbf7f16c18ce0109ddf24
Gustavo Rodrigues <127614876+devGusta2@users.noreply.github.com>
2026-02-08 00:56:42 -0300

    Importando safe area contex
    Agora é possível com que no celular o espaço da interface do app não invada a navbar ou status bar do dispositivo.

--- MentecMobile/App.js
+++ MentecMobile/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
     <AuthProvider>
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </AuthProvider>
   );
 }
