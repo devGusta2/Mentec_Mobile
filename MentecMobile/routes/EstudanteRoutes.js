e3c215aa57121217417f4469264088268c2e24ba
SabrinaSilva1 <142277089+SabrinaSilva1@users.noreply.github.com>
2026-05-17 20:29:44 -0300

    Fórum atualizado
    Front end do Fórum conectado com o back end

--- MentecMobile/routes/EstudanteRoutes.js
+++ MentecMobile/routes/EstudanteRoutes.js
@@ -6,6 +6,7 @@ import ListaMonitorias from "../Screens/ListaMonitorias";
 // import Forum from "../Screens/Forum";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HistoricoMonitorias from "../Screens/HistoricoMonitorias";
+import Forum from "../Screens/Forum";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +18,7 @@ export function EstudanteRoutes() {
       <Stack.Screen name="Cronograma" component={Cronograma} />
       <Stack.Screen name="Perfil" component={Perfil} />
       <Stack.Screen name="HistoricoMonitorias" component={HistoricoMonitorias} />
-      {/* <Stack.Screen name="Forum" component={Forum} /> */}
+      <Stack.Screen name="Forum" component={Forum} />
 
     </Stack.Navigator>
   );
